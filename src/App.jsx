928e8116d96622db134489c53beb5158a0ec537d
gngu44 <197215360+gngu44@users.noreply.github.com>
2025-08-14 23:55:49 -0400

    css changes css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import './App.css'
+import './index.css'
 
 function App() {
-  return (
-    <>
-      
-    </>
-  )
+  return <div className="app-container"> Hola </div>;
 }
 
 export default App
