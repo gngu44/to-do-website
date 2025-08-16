cae4886fe097820ca5035fcbd1c267ef424536a7
gngu44 <197215360+gngu44@users.noreply.github.com>
2025-08-15 23:58:09 -0400

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './index.css'
 
 function App() {
-  return <div className="app-container"> Hola </div>;
+  return (
+  <div className="app-container"> 
+    <h2> To-Do List</h2>
+    <div>
+
+
+    </div>
+  </div>
+
+  
+
+  )
 }
 
 export default App
